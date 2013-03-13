398e187a4bf9ba17811b3ec616c278a84530bbb6
wuyuxuan <wuyuxuan2014@gmail.com>
2013-03-14 00:16:28 +0800

    add support for multiple data source

--- server/car-service-core/src/test/java/com/huhuo/carservicecore/csm/charge/DaoChargeStandardTest.java
+++ server/car-service-core/src/test/java/com/huhuo/carservicecore/csm/charge/DaoChargeStandardTest.java
@@ -7,6 +7,8 @@ import javax.annotation.Resource;
 import org.junit.Test;
 
 import com.huhuo.carservicecore.BaseTest;
+import com.huhuo.carservicecore.db.DataSourceContextHolder;
+import com.huhuo.carservicecore.db.DataSourceType;
 
 public class DaoChargeStandardTest extends BaseTest {
 
@@ -23,6 +25,16 @@ public class DaoChargeStandardTest extends BaseTest {
 	public void findModels() {
 		List<ModelChargeStandard> list = iDaoChargeStandard.findModels(0, 20);
 		print(list);
+	}
+	
+	@Test
+	public void routingDSTest() {
+		// test multiple data source
+		DataSourceContextHolder.setDataSourceType(DataSourceType.CUSTOMER_CHITONG);
+		print(iDaoChargeStandard.findModels(0, 10));
+		// another data source
+		DataSourceContextHolder.setDataSourceType(DataSourceType.CUSTOMER_2);
+		print(iDaoChargeStandard.findModels(0, 10));
 	}
 	
 }
