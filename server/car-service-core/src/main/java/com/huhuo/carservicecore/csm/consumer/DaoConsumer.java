2d53e9ec317c2d3e5d83e6c2ae52a4d66d531203
wuyuxuan <wuyuxuan2014@gmail.com>
2013-03-16 18:15:40 +0800

    resolve SQL insert bug caused by the external Object field in model

--- server/car-service-core/src/main/java/com/huhuo/carservicecore/csm/consumer/DaoConsumer.java
+++ server/car-service-core/src/main/java/com/huhuo/carservicecore/csm/consumer/DaoConsumer.java
@@ -1,12 +1,23 @@
 package com.huhuo.carservicecore.csm.consumer;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Repository;
 
+import com.huhuo.carservicecore.constant.Dictionary.DictGroup;
 import com.huhuo.carservicecore.db.GenericBaseExtenseDao;
+import com.huhuo.carservicecore.sys.dictionary.IDaoDictionary;
+import com.huhuo.carservicecore.sys.dictionary.ModelDictionary;
+import com.huhuo.integration.db.mysql.Condition;
 
 @Repository("carservicecoreDaoConsumer")
 public class DaoConsumer extends GenericBaseExtenseDao<ModelConsumer> implements IDaoConsumer {
 
+	@Resource(name = "carservicecoreDaoDictionary")
+	private IDaoDictionary<ModelDictionary> iDaoDictionary;
+	
 	@Override
 	public String getTableName() {
 		return "csm_consumer";
@@ -16,5 +27,18 @@ public class DaoConsumer extends GenericBaseExtenseDao<ModelConsumer> implements
 	public Class<ModelConsumer> getModelClazz() {
 		return ModelConsumer.class;
 	}
+
+	@Override
+	public List<ModelConsumer> findByCondition(
+			Condition<ModelConsumer> condition) {
+		// TODO Auto-generated method stub
+		List<ModelConsumer> list = super.findByCondition(condition);
+		for(ModelConsumer consumer : list) {
+			ModelDictionary genderDic = iDaoDictionary.getBy(DictGroup.GENERAL_GENDER, consumer.getGender());
+			consumer.setGenderDic(genderDic);
+		}
+		return list;
+	}
+	
 
 }
